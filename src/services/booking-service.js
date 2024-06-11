e055a0d409698d5822691d5ddaf68e2e7e48eaa7
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-11 12:49:25 +0530

    11th June 2024

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -16,18 +16,17 @@ class BookingService {
             const response = await axios.get(getFlightRequestURL);
             const flightData = response.data.data;
             let priceOfTheFlight = flightData.price;
-            // if(data.noOfSeats > flightData.totalSeats) {
-            //     throw new ServiceError('Something went wrong in the booking process', 'Insufficient seats in the flight');
-            // }
-            // const totalCost = priceOfTheFlight * data.noOfSeats;
-            // const bookingPayload = {...data, totalCost};
-            // const booking = await this.bookingRepository.create(bookingPayload);
-            // const updateFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${booking.flightId}`;
+            if(data.noOfSeats > flightData.totalSeats) {
+                throw new ServiceError('Something went wrong in the booking process', 'Insufficient seats in the flight');
+            }
+            const totalCost = priceOfTheFlight * data.noOfSeats;
+            const bookingPayload = {...data, totalCost};
+            const booking = await this.bookingRepository.create(bookingPayload);
+            const updateFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${booking.flightId}`;
             // console.log(updateFlightRequestURL);
-            // await axios.patch(updateFlightRequestURL, {totalSeats: flightData.totalSeats - booking.noOfSeats});
-            // const finalBooking = await this.bookingRepository.update(booking.id, {status: "Booked"});
-            // return finalBooking;
-            return flightData;
+            await axios.patch(updateFlightRequestURL, {totalSeats: flightData.totalSeats - booking.noOfSeats});
+            const finalBooking = await this.bookingRepository.update(booking.id, {status: "Booked"});
+            return finalBooking;
         } catch (error) { 
             console.log(error);
             if(error.name == 'RepositoryError' || error.name == 'ValidationError') {
