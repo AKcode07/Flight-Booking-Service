bb4e28c38536ee7fa1b5a3c995874008ac66d721
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-16 11:42:59 +0530

    16th June 2024

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const setupAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
   app.use('/api', apiRoutes);
-
+  
   app.listen(PORT, () => {
     console.log(`Server start on Port ${PORT}`);
     if (process.env.DB_SYNC) {
